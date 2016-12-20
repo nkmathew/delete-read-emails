5719a1a40302c9a4b67c6c1c5e33ed74709def85
nkmathew <kipkoechmathew@gmail.com>
2016-12-20 12:30:40 +0300

    Add logic for selecting the read emails and deleting them

--- chrome/content/button.js
+++ chrome/content/button.js
@@ -2,10 +2,40 @@
  * Delete Read Emails
  *
  * @date 19th December 2016
+ * @author nkmathew
  *
  */
+
+/**
+ * Poor man's debugger
+ * Displays all the attributes of the object
+ *
+ * For when you don't feel like setting up the debugger in Firefox
+ *
+ */
+/* exported inspect */
+function inspect(object) {
+  alert('Found: ' + Object.keys(object).length + ' Type: ' + typeof object);
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      alert(key + ' = ' + object[key]);
+    }
+  }
+}
+
+/**
+ * Toolbar button callback function
+ */
+/* exported deleteReadEmails */
 function deleteReadEmails() {
-  alert('All systems functioning');
-  console.log('Thunderbird console log test...')
-  goDoCommand('print');
+  let treeView = gDBView.QueryInterface(Ci.nsITreeView);
+  let count = treeView.rowCount;
+  alert('Email Count: ' + count);
+  for (let i = 0; i < count; i++) {
+    let email = messenger.msgHdrFromURI(gDBView.getURIForViewIndex(i));
+    if (email.isRead) {
+      treeView.selection.rangedSelect(i, i, true);
+    }
+  }
+  goDoCommand('cmd_delete');
 }
