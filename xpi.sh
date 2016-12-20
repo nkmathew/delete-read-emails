c25de01a206229f52a8f58d082a934f47d098926
nkmathew <kipkoechmathew@gmail.com>
2016-12-20 12:30:40 +0300

    Notify when the generated XPI does not install remotely in Thunderbird

--- xpi.sh
+++ xpi.sh
@@ -13,10 +13,13 @@ sys.stdout.write(version)
 EOF
 )
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 # Package the extension
 xpi_name="delete-read-emails@nkmathew.net.$version.xpi"
 rm -f $xpi_name
-7z a $xpi_name install.rdf chrome.manifest chrome/ | grep Compressing
+7z a $xpi_name install.rdf chrome.manifest chrome/ defaults/ | grep Compressing
 
 # Output the xpi size and name
 xpi_size=$(stat -c%s $xpi_name)
@@ -25,5 +28,8 @@ echo -e "\nSize: ${xpi_size}KB\nName: $xpi_name"
 
 # Don't restart Thunderbird if flag "-n" is passed
 if [[ ! $1 == -n ]]; then
-  wget --quiet --post-file=$xpi_name http://127.0.0.1:8888/
+  response=$(wget --post-file=$xpi_name http://127.0.0.1:8888/ 2>&1 | grep failed)
+  if [[ ! -z $response ]]; then
+    echo -e "${RED}Failed to install the xpi remotely${NC}"
+  fi
 fi
